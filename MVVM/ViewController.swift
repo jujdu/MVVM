20ca2c1bc631b783291c97e785fdaa05d01d28ae
jujdu <michael.sidoruk@icloud.com>
2019-09-13 23:37:51 +0300

    initial commint

---
+++ MVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM
+//
+//  Created by Michael Sidoruk on 13/09/2019.
+//  Copyright © 2019 Michael Sidoruk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
